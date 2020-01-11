a438cf9c15d4614b9d335739a359e130dbd91011
StFog <stephane.foglio@orange.fr>
2020-01-11 16:08:17 +0100

    add philips hue api

--- games/dev.js
+++ games/dev.js
@@ -1,5 +1,33 @@
 const { withHermes } = require('hermes-javascript')
+const v3 = require('node-hue-api').v3;
+const LightState = v3.lightStates.LightState;
+
+
 //****** Dev Game ******//
+
+const USERNAME = 'your username to authenticating with the bridge'
+  // The name of the light we wish to retrieve by name
+  , LIGHT_ID = 2
+;
+
+v3.discovery.nupnpSearch()
+  .then(searchResults => {
+    const host = searchResults[0].ipaddress;
+    return v3.api.createInsecureLocal(host).connect(USERNAME);
+  })
+  .then(api => {
+    // Using a LightState object to build the desired state
+    const state = new LightState()
+      .on()
+      .ct(200)
+      .brightness(100)
+    ;
+    return api.lights.setLightState(LIGHT_ID, state);
+  })
+  .then(result => {
+    console.log(`Light state change was successful? ${result}`);
+  })
+;
 
 //****** from Odas ******//
 
